a97818e09493b443bbbb96dc001a4c4028a6dc7a
Tamar Mekhrishvili <tamar.mekhrishvili@medsoft.ge>
2020-12-20 17:21:23 +0400

    routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import './css/index.css';
 import './css/reset.css';
+import Home from './components/home/Home';
+import Item from './components/item/Item';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 const App = () => {
   return (
-    <div>
-
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/item">
+          <Item />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
