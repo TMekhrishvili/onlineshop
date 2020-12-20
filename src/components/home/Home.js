05131c6d6764af017e2d1a214fb6fdb674b5b1b7
Tamar Mekhrishvili <tamar.mekhrishvili@medsoft.ge>
2020-12-20 17:59:35 +0400

    fluid containers

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from 'react';
+import './home.css';
 
 const Home = () => {
     return (
-        <div>
+        <div className="home-container">
+            <div className="categories">
+                <div className="price">
+                    <div className="price-title">
+                        <h2 className="title">Price</h2>
+                    </div>
+                    <div className="choose-price">
 
+                    </div>
+                </div>
+                <div className="brand">
+                    <div className="brand-title">
+                        <h2 className="title">Brand</h2>
+                    </div>
+                    <div className="choose-brand">
+                        
+                    </div>
+                </div>
+            </div>
+            <div className="items">
+            </div>
         </div>
     )
 }
